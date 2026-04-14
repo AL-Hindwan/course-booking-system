48ccc868305f91fcb78c5cf4058de29eb22eb26a
Hammam <hmaamalhndwan@gmail.com>
2026-04-14 13:24:21 -0900

    Fix final deployment size: optimized Prisma engines

--- auth-backend/src/config/index.ts
+++ auth-backend/src/config/index.ts
@@ -39,7 +39,7 @@ export const config: Config = {
     nodeEnv: process.env.NODE_ENV || 'development',
 
     database: {
-        url: process.env.DATABASE_URL!,
+        url: process.env.DATABASE_URL || '',
     },
 
     jwt: {
